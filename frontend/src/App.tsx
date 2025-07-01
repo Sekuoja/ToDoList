55fc7ca495d3d30005fe3485df96236b4e2bf411
sekuoja <krokus.xx@yandex.ru>
2025-07-01 10:23:45 +0000

    input

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import { Input, Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
+const App = () => {
+    returb <Input.Group><Input placeholder="Search.."/><Button type="primary" icon={<SearchOutlined />}/></Input.Group>
+}
+
+export default App;
