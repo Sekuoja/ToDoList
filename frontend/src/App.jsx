9d7528986e610ca651f42d663fe6e3b9b85332f1
sequoia <krokus.xx@yandex.ru>
2025-07-02 18:56:56 +0300

    add List

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
-import { Input, Button } from "antd";
+import { Input, Button, List } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
+
+const data = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+];
+
 
 const App = () => {
     return <>
- <Input.Group><Input placeholder="Search.."/><Button type="primary" style={{flexDirection: "row-reverse", gap: "8px"}} icon={<SearchOutlined />}>Search</Button></Input.Group>
-           </>
+        <Input.Group><Input placeholder="Search.." /><Button type="primary" style={{ flexDirection: "row-reverse", gap: "8px" }} icon={<SearchOutlined />}>Search</Button></Input.Group>
+        <List
+            header={<div>Header</div>}
+            footer={<div>Footer</div>}
+            bordered
+            dataSource={data}
+            renderItem={(item) => (
+                <List.Item>
+                    {item}
+                </List.Item>
+            )} />
+    </>
 }
 
 export default App;
